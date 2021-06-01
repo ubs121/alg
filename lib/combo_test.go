e7d240cbc967b6ca8de96d462a726066880d9afd
ubs121 <ubs121@gmail.com>
2021-06-01 17:34:08 -0400

    combo fix n==k

--- lib/combo_test.go
+++ lib/combo_test.go
@@ -18,12 +18,13 @@ func combine(n int, k int) [][]int {
 
 	var ret [][]int
 
+	// collect it
+	cmb := make([]int, k)
+	copy(cmb, a[1:])
+	ret = append(ret, cmb)
+
 	p := k
-	for p > 0 {
-		/* шинэ байрлал */
-		cmb := make([]int, k)
-		copy(cmb, a[1:])
-		ret = append(ret, cmb[:])
+	for a[1] < n-k+1 {
 
 		/* дараагийн хэсэглэлийг зохиох */
 		if a[k] == n {
@@ -32,19 +33,22 @@ func combine(n int, k int) [][]int {
 			p = k
 		}
 
-		if p > 0 {
-			for i := k; i >= p; i-- {
-				a[i] = a[p] + (i - p + 1)
-			}
+		for i := k; i >= p; i-- {
+			a[i] = a[p] + (i - p + 1)
 		}
+
+		/* шинэ байрлал */
+		cmb := make([]int, k)
+		copy(cmb, a[1:])
+		ret = append(ret, cmb)
 	}
 
 	return ret
 }
 
 func TestCombo(t *testing.T) {
-	n := 20
-	k := 5
+	n := 10
+	k := 10
 	ret := combine(n, k)
 	if len(ret) != fact(n)/(fact(n-k)*fact(k)) {
 		t.Errorf("can't match")
@@ -52,10 +56,53 @@ func TestCombo(t *testing.T) {
 	fmt.Printf("%v", ret)
 }
 
+func TestComboN(t *testing.T) {
+	n := 20
+	facts := map[int]int{}
+	facts[1] = 1
+	facts[2] = 2
+	for i := 3; i <= n; i++ {
+		facts[i] = fact(i)
+	}
+
+	fmt.Printf("%v", facts)
+}
 func fact(n int) int {
 	p := 1
 	for i := 2; i <= n; i++ {
 		p = p * i
 	}
 	return p
+}
+
+func combine2(n int, k int) [][]int {
+	a := make([]int, k)
+
+	// initialize first combination
+	for i := 0; i < k; i++ {
+		a[i] = i
+	}
+
+	i := k - 1
+
+	var ret [][]int
+
+	for a[0] < n-k+1 {
+		for i > 0 && a[i] == n-k+i {
+			i--
+		}
+
+		// collect it
+		cmb := make([]int, k)
+		copy(cmb, a)
+		ret = append(ret, cmb)
+
+		a[i]++
+
+		for i < k-1 {
+			a[i+1] = a[i] + 1
+			i++
+		}
+	}
+	return ret
 }
