142f8d528bbe1b7b8b7bb6ba5cd7934174c0a749
ubs121 <ubs121@gmail.com>
2021-06-02 22:34:58 -0400

    permute, hash anagram, rotate matrix

--- lib/permute_test.go
+++ lib/permute_test.go
@@ -2,30 +2,38 @@
 package alg
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestPermute(t *testing.T) {
-	a := []int{1, 2, 3} /* сэлгэх утгуудыг агуулах массив */
-	N := len(a)         /* сэлгэх элементийн тоо */
+func permute(nums []int) [][]int {
+	n := len(nums)
 
-	display(a)
+	var ret [][]int
 
-	p := make([]int, N) /* сэлгэмлийг удирдах массив */
-	var j int
+	// output initial state
+	cmb := make([]int, n)
+	copy(cmb, nums)
+	ret = append(ret, cmb)
 
-	for i := 1; i < N; {
+	// an encoding of the stack state.
+	p := make([]int, n)
+
+	i := 1
+	for i < n {
 		if p[i] < i {
+			j := 0
 			if i%2 > 0 {
 				j = p[i]
-			} else {
-				j = 0
 			}
-			// swap
-			a[j], a[i] = a[i], a[j]
 
-			/* сэлгэмлийн  шинэ хувилбарыг хэвлэх */
-			display(a)
+			// swap
+			nums[j], nums[i] = nums[i], nums[j]
+
+			// output
+			cmb := make([]int, n)
+			copy(cmb, nums)
+			ret = append(ret, cmb)
 
 			p[i]++
 			i = 1
@@ -34,12 +42,47 @@ func TestPermute(t *testing.T) {
 			i++
 		}
 	}
+	return ret
 }
 
-// туслах функц
-func display(arr []int) {
-	for i := 0; i < len(arr); i++ {
-		print(arr[i], " ")
+func TestPermute(t *testing.T) {
+	a := []int{1, 2, 3} /* сэлгэх утгуудыг агуулах массив */
+	ret := permute(a)
+	fmt.Printf("%v", ret)
+}
+
+// recursive version
+func permuteRecurse(k int, nums []int, out chan []int) {
+	if k == 1 {
+		// output
+		cmb := make([]int, len(nums))
+		copy(cmb, nums)
+		out <- cmb
+	} else {
+		permuteRecurse(k-1, nums, out)
+
+		for i := 0; i < k-1; i++ {
+			if k%2 == 0 {
+				nums[i], nums[k-1] = nums[k-1], nums[i]
+			} else {
+				nums[0], nums[k-1] = nums[k-1], nums[0]
+			}
+
+			permuteRecurse(k-1, nums, out)
+		}
 	}
-	println()
+}
+
+func TestPermute2(t *testing.T) {
+	a := []int{1, 2, 3} /* сэлгэх утгуудыг агуулах массив */
+
+	out := make(chan []int)
+	go func() {
+		defer close(out)
+		permuteRecurse(len(a), a, out)
+	}()
+
+	for v := range out {
+		fmt.Printf("%v\n", v)
+	}
 }
